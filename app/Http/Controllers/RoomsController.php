35334ee675b091b3add611697ac5101eb3de32b6
Callum D E Smith <callumdesmith@callumdesmith.co.uk>
2018-04-13 12:15:31 +0100

    Added room availablility query to rooms controller.

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -7,8 +7,19 @@ use Illuminate\Http\Request;
 class RoomsController extends Controller
 {
     //
-    public function checkAvailableRooms()
+    public function checkAvailableRooms($client_id, Request $request)
     {
+        $dateFrom = $request->input('dateFrom'); 
+        $dateTo = $request->input('dateTo');
+        $client = new Client();
+        $room = new Room();
+
+        $data = [];
+        $date['dateFrom'] = $dateFrom;
+        $date['dateTo'] = $dateTo;
+        $data['rooms'] = $room->getAvailabeRooms($dateFrom, $dateTo);
+        $data['client'] = $client->find($client_id);
+        
         return view('rooms/checkAvailableRooms');
     }
 }
