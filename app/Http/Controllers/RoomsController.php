e47d584aa5b34996d2b90dfd6449f870096aa54f
Callum D E Smith <callumdesmith@callumdesmith.co.uk>
2018-04-13 12:32:03 +0100

    Return room availablity, input from form.

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -3,23 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Client;
+use App\Room;
 
 class RoomsController extends Controller
 {
     //
     public function checkAvailableRooms($client_id, Request $request)
     {
-        $dateFrom = $request->input('dateFrom'); 
+        $dateFrom = $request->input('dateFrom');
         $dateTo = $request->input('dateTo');
         $client = new Client();
         $room = new Room();
 
         $data = [];
-        $date['dateFrom'] = $dateFrom;
-        $date['dateTo'] = $dateTo;
-        $data['rooms'] = $room->getAvailabeRooms($dateFrom, $dateTo);
+        $data['dateFrom'] = $dateFrom;
+        $data['dateTo'] = $dateTo;
+        $data['rooms']= $room->getAvailableRooms($dateFrom, $dateTo);
         $data['client'] = $client->find($client_id);
-        
-        return view('rooms/checkAvailableRooms');
+
+        return view('rooms/checkAvailableRooms', $data);
     }
 }
