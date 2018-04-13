9f861cf37c2a71985cddb925611ba537a36af80e
Callum D E Smith <callumdesmith@callumdesmith.co.uk>
2018-04-13 13:58:31 +0100

    Added professor element to titles model and test titles count and last element.

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Title;
 
 class ExampleTest extends TestCase
 {
@@ -16,5 +17,18 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $this->assertTrue(true);
+    }
+
+    public function testTitlesModelCount()
+    {
+        $titles = new Title;
+        $this->assertTrue( count( $titles->all() ) === 6, 'It should have 6 titles.');
+    }
+
+    public function testLastTitleShouldProfessor()
+    {
+        $titles = new Title;
+        $titles_array = $titles->all();
+        $this->assertEquals( 'Professor', array_pop( $titles_array ), 'The last element should be professor.' );
     }
 }
